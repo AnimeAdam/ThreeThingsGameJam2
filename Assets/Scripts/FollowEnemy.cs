46d87fb663348b11db31f6be0b89c0f82b8cb05a
XxdarsanxX <darsan.muhamad@gmail.com>
2024-10-11 22:00:36 +0100

    followEnemyFirst

---
+++ Assets/Scripts/FollowEnemy.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FollowEnemy : MonoBehaviour
+{
+    private GameObject enemy;
+    public bool hasSeenEnemy ;
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasSeenEnemy = false;
+        speed = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasSeenEnemy) { 
+        followEnemy(enemy.gameObject.transform.position);
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            setEnemy(collision.gameObject);
+
+        }
+        else if (collision.gameObject.tag == "Enemy")
+        {
+           setEnemy(collision.gameObject);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            hasSeenEnemy = false;
+        }
+        else if (collision.gameObject.tag == "Enemy")
+        {
+            hasSeenEnemy = false;
+        }
+    }
+    private void setEnemy(GameObject myEnemy)
+    {
+        hasSeenEnemy = true;
+        enemy = myEnemy;
+    }
+    private void followEnemy(Vector2 pos)
+    {
+        float dist = Vector3.Distance(pos, transform.position);
+        if(dist > 1)
+            transform.position = Vector2.MoveTowards(transform.position, pos, speed * Time.deltaTime);
+
+    }
+}
