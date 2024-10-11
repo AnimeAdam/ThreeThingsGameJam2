295597e832bc9a0d682c4268eb6e2f19167051f2
AnimeAdam <Adam.Szlamp@gmail.com>
2024-10-11 19:56:40 +0100

    Project resetted to not be fucked UP.

---
+++ Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public int maxHealth = 3;
+    public int currentHealth;
+    public float movementSpeed = 2f;
+    public UnitType enemyType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ApplyDamage(int damage)
+    {
+        currentHealth -= damage;
+        CheckIfDead();
+    }
+
+    void CheckIfDead()
+    {
+        if (currentHealth <= 0){
+            gameObject.SendMessage("DestoryThisEnemy");
+        }
+
+    }
+}
